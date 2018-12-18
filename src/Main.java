69147d722e120198f12f3e4e66ede8a9a3e96525
Mark Young <young.mark@wgcloud.org>
2018-12-18 19:45:42 +0000

    Commit #2 12/18/2018

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ public class Main {
     
     public static void main(String[]args)
     {
+        Sports sp1 = new Sports("Baseball");
         
+        System.out.println(sp1);
     }
 }
--- src/Sports.java
+++ src/Sports.java
@@ -1,3 +1,18 @@
 
 public class Sports {
+
+    //instance variables
+    String sportName;
+
+    public Sports(String nm)
+    {
+        sportName = nm;
+    }
+    
+    public String toString()
+    {
+        return "Your sport is called " + sportName;
+    }
 }
+
+
